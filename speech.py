f033088c10ccfdd810e08fde9251242c4d104a6a
Gaurav Jain <crazcuber@gmail.com>
2018-05-27 12:47:32 +0530

    did some basics

--- speech.py
+++ speech.py
@@ -2,12 +2,11 @@ import speech_recognition as sr
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-print(sr.Microphone.list_microphone_names())
-'''
-harvard = sr.AudioFile('amy.wav')
-with harvard as source:
+with mic as source:
     r.adjust_for_ambient_noise(source)
-    audio = r.record(source)
+    audio = r.listen(source)
 
-print(r.recognize_google(audio,show_all=True))
-'''
+try:
+    print(r.recognize_google(audio,show_all=True))
+except:
+    print('error happend :(')
